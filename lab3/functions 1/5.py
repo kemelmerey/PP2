87797f25e3618046f2e57db7a43941952e923fec
Merey <kemelm2004@gmail.com>
2024-02-10 02:11:56 +0600

    ex: 5

---
+++ lab3/functions 1/5.py
@@ -0,0 +1,10 @@
+#Write a function that accepts string from user and print all permutations of that string.
+from itertools import permutations
+input = input("Input string: ")
+
+def string_permutations(str):
+    permutation_list = permutations(str)
+    for permutation in permutation_list:
+        print("".join(permutation))
+
+string_permutations(input)
